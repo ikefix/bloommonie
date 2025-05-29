48ab742264aa8167fc756a7a95721b6ab2a468e9
ikefix <omekeikechukwu877@gmail.com>
2025-05-29 23:53:32 +0100

    day2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 
     <!-- Scripts -->
     <link rel="stylesheet" href="resources/css/app.css">
-        @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css'])
+        {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css']) --}}
 </head>
 <body>
     <div id="app">
