90888841cc95cd81046c08efb232819e5ef62e28
ikefix <omekeikechukwu877@gmail.com>
2025-05-30 11:52:52 +0100

    appblade is problem

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 
     <!-- Scripts -->
     <link rel="stylesheet" href="resources/css/app.css">
-        @vite(['resources/sass/app.scss', 'resources/js/app.js',])
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     <div id="app">
