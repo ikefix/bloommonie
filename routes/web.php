4385e9035c162fc40962c6b7dec8bec8b2265df9
ikefix <omekeikechukwu877@gmail.com>
2025-06-04 16:31:17 +0100

    worked on login and register form

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CompanyRegistrationController;
 use App\Http\Controllers\PaymentController;
-
+use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
